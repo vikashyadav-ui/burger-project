e95fdc27dfada4b015a0ba0a923287b18197f005
vikashyadav-ui <vikashdasari.96@gmail.com>
2020-05-03 11:30:20 +0530

    Updates

---
+++ precache-manifest.8aee234e246dec6f7b7aeda072c401d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84100a60313a9e1931b666ff90299b6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256a804d7fd79b14b6f0",
+    "url": "/static/css/main.f4eff14d.chunk.css"
+  },
+  {
+    "revision": "e22cb5a11405c29b23ac",
+    "url": "/static/js/2.6d604a8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d604a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256a804d7fd79b14b6f0",
+    "url": "/static/js/main.70fd97ff.chunk.js"
+  },
+  {
+    "revision": "a61b27b02161d6e4f62a",
+    "url": "/static/js/runtime-main.90b4a667.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/original.b8503d26.png"
+  }
+]);
